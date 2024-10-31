a361277508fff25158bade6b8052cbdce4162d04
imvne <imanebeaufils19@gmail.com>
2024-10-31 13:20:47 +0100

    initial commit

---
+++ odin-tictactoe-reac/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+export default function App(){
+	
+	return (
+	<>
+		<div className="which-players-turn">
+			<p className="x-player player-turn">x</p>
+			<p className="o-player player-turn">o</p>
+		</div>
+	</>
+	)
+	
+	
+}
