c47195a9b87a7c816f68607377f4c2ac88078755
imvne <imanebeaufils19@gmail.com>
2024-11-02 19:52:18 +0100

    ajout fichier pour images

---
+++ odin-memory-card-react/src/assets/charactersImages.js
@@ -0,0 +1,29 @@
+import onigiri from './onigiri.png'
+import skull from './skull.png'
+import flower from './flower.jpg'
+import frog from './frog.png'
+import notes from './notes.jpg'
+import kitten from './kitten.jpg'
+import russiandoll from './russiandoll.png'
+import champi from './champi.jpg'
+import ghost from './ghost.jpg'
+import tangerine from './tangerine.jpg'
+import spider from './spider.jpg'
+import star from './star.jpg'
+
+const charactersImages = {
+	onigiri,
+	skull,
+	flower,
+	frog,
+	notes,
+	kitten,
+	russiandoll,
+	champi,
+	ghost,
+	tangerine,
+	spider,
+	star,
+}
+
+export default charactersImages
