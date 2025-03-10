364d15140a758ec41e8b07f6f8bff42bb62d44fb
imvne <imanebeaufils19@gmail.com>
2025-03-10 16:11:07 +0100

    tsx

---
+++ shopping-cart/src/types/CartData.tsx
@@ -0,0 +1,12 @@
+// import CartProductType from "./CartProductType"
+
+// type CartData = {
+// 	productData: CartProductType,
+// 	cartContent: [], 
+// 	deleteProductFromCart: (productId: number) => void, 
+// 	addProductAmount: (productId: number) => void, 
+// 	subProductAmount: (productId: number) => void, 
+// 	addProductToCart: (productId: number) => void, 
+// }
+
+// export default CartData
