ef49b75306c105fe3de4d8b53a24516391d0f603
imvne <imanebeaufils19@gmail.com>
2025-03-10 16:08:34 +0100

    fichiers

---
+++ shopping-cart/src/assets/productsData.tsx
@@ -0,0 +1,78 @@
+import blue from './blue.png'
+import blue1 from './blue1.png'
+import green from './green.png'
+import orange from './orange.png'
+import violet from './violet.png'
+import yellow from './yellow.png'
+
+const Products = [
+	{
+		id: 1,
+		name: "x-12E",
+		price: 15,
+		weight: 1.5,
+		weightUnity: "mg",
+		money: "$",
+		img: blue,
+		description: "A burst of electric euphoria, this pill sends waves of neon energy through your veins, making the world pulse in sync with your heartbeat.",
+		amount: 1,
+		
+	},
+	{
+		id: 2,
+		name: "x-PL3",
+		price: 25,
+		weight: 1.5,
+		weightUnity: "mg",
+		money: "$",
+		img: blue1,
+		description: "A dreamy drift into cosmic bliss, melting away stress and gravity, leaving you floating weightless in a universe of pure color.",
+		amount: 1,
+	},
+	{
+		id: 3,
+		name: "x-40P",
+		price: 36,
+		weight: 1.5,
+		weightUnity: "mg",
+		money: "$",
+		img: green,
+		description: "Lightning in a capsule, igniting your mind with razor-sharp focus and limitless creativity—ideas spark and explode like fireworks.",
+		amount: 1,
+	},
+	{
+		id: 4,
+		name: "x-12A",
+		price: 12,
+		weight: 1.5,
+		weightUnity: "mg",
+		money: "$",
+		img: orange,
+		description: "A candy-coated rush of wild joy, flipping reality into a funhouse mirror where everything is hilarious, vibrant, and full of possibility.",
+		amount: 1,
+	},
+	{
+		id: 5,
+		name: "x-39O",
+		price: 10,
+		weight: 1.5,
+		weightUnity: "mg",
+		money: "$",
+		img: violet,
+		description: "A warm, golden glow from the inside out, wrapping you in pure love, deep connection, and a feeling that everything is exactly as it should be.",
+		amount: 1,
+	},
+	{
+		id: 6,
+		name: "x-19I",
+		price: 11,
+		weight: 1.5,
+		weightUnity: "mg",
+		money: "$",
+		img: yellow,
+		description: "A slow-motion dive into liquid serenity, washing away anxiety with rolling waves of calm, clarity, and effortless confidence.",
+		amount: 1,
+	},
+]
+
+export default Products
